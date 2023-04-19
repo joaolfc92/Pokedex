fadb56e1aedb7ed1dc0018f49d50bec3d832a673
joaolfc92 <joao_luiz92@hotmail.com>
2023-04-19 10:12:53 -0300

    inicio js API feito ate o tipo

--- scipt/script.js
+++ scipt/script.js
@@ -1,14 +1,81 @@
-const search = document.querySelector("#search");
+const search        = document.querySelector("#search");
+const number        = document.querySelector('#number');
+const pokemonImage  = document.querySelector("#pokemon-image");
+const type          = document.querySelectorAll(".type");
+const types          = document.querySelector("#types");
+
+const typeColors = {
+    "rock":     [182, 158,  49],
+    "ghost":    [112,  85, 155],
+    "steel":    [183, 185, 208],
+    "water":    [100, 147, 235],
+    "grass":    [116, 203,  72],
+    "psychic":  [251,  85, 132],
+    "ice":      [154, 214, 223],
+    "dark":     [117,  87,  76],
+    "fairy":    [230, 158, 172],
+    "normal":   [170, 166, 127],
+    "fighting": [193,  34,  57],
+    "flying":   [168, 145, 236],
+    "poison":   [164,  62, 158],
+    "ground":   [222, 193, 107],
+    "bug":      [167, 183,  35],
+    "fire":     [245, 125,  49],
+    "electric": [249, 207,  48],
+    "dragon":   [112,  55, 255]
+}
+
+
 
 const fetchApi = async (pkmnName) => {
-   const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + pkmnName);
-   const pknmData = await response.json();
+    pkmnNameApi = pkmnName.split(' ').join('-')
 
-   console.log(pknmData)
+
+   const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + pkmnNameApi);
+
+    if(response.status === 200){
+        const pknmData = await response.json();
+        return pknmData;
+    }
+
+    return false;
+
+
+  
 };
 
 search.addEventListener('change', async (event)=>{
-    await fetchApi(event.target.value)
+   const pkmnData =  await fetchApi(event.target.value)
+
+   //validation when pokemon does not exist
+
+   if(!pkmnData) alert("Pokemon não registrado");
+
+
+   // for debugging - will be removed later on
+   console.log(pkmnData);
+
+   // set pokemon # at the top of the page 
+   number.innerHTML = '#' + pkmnData.id.toString().padStart(3, '0');
+
+   // set pokemon image
+   pokemonImage.src = pkmnData.sprites.other.home.front_default;
+
+   //updates "type"
+    types.innerHTML = '';
+
+    pkmnData.types.forEach((t)=>{
+    let newType = document.createElement("span");
+    newType.innerHTML = t.type.name;
+    newType.classList.add("type");
+    newType.style.backgroundColor = 'rgb($ {typeColors[t.type.name][0]}, $ {typeColors[t.type.name][1]}, $ {typeColors[t.type.name][2]})';
+    
+
+    
+    types.appendChild(newType);
+
+    })
+
 });
 
 
